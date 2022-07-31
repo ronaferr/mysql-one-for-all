9d66a7dbe31c985d256ff7bad416a94387d12791
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-07-31 13:38:56 -0300

    query para exibir quantidade de musicas no historico de usuario

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+SELECT
+	COUNT(h.usuario_id) AS quantidade_musicas_no_historico
+FROM
+	SpotifyClone.historico AS h
+INNER JOIN
+	SpotifyClone.usuarios AS u
+    ON h.usuario_id = u.usuario_id
+WHERE
+	u.usuario = 'Bill';
+  
